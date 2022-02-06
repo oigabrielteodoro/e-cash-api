23db37ef69544475061cb196fa45b3ba3b4cd7fe
Gabriel Teodoro <oi@gabrielteodoro.com>
2022-02-06 16:35:14 -0300

    Add connection with database by env

---
+++ src/config/database.ts
@@ -0,0 +1,5 @@
+export const DATABASE_PORT = Number(process.env.DATABASE_PORT)
+export const DATABASE_NAME = String(process.env.DATABASE_NAME)
+export const DATABASE_HOST = String(process.env.DATABASE_HOST)
+export const DATABASE_USERNAME = String(process.env.DATABASE_USERNAME)
+export const DATABASE_PASSWORD = String(process.env.DATABASE_PASSWORD)
