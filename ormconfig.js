88acefca736206adce0414fc61b8a88c0a040fc5
Gabriel Teodoro <oi@gabrielteodoro.com>
2022-02-07 17:49:08 -0300

    Add heroku configuration

--- ormconfig.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  type: 'postgres',
-  url: process.env.DATABASE_URL,
-  entities: [process.env.DATABASE_ENTITIES],
-  migrations: [process.env.DATABASE_MIGRATIONS],
-  cli: {
-    migrationsDir: './src/database/migrations',
-  },
-}
--- src/database/index.ts
+++ src/database/index.ts
@@ -1,3 +1,11 @@
 import { createConnection } from 'typeorm'
 
-createConnection()
+createConnection({
+  type: 'postgres',
+  url: process.env.DATABASE_URL,
+  entities: [process.env.DATABASE_ENTITIES],
+  migrations: [process.env.DATABASE_MIGRATIONS],
+  cli: {
+    migrationsDir: './src/database/migrations',
+  },
+})
