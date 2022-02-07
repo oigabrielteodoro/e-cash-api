551a74b7198f69f585e898756e5169cd794a907f
Gabriel Teodoro <oi@gabrielteodoro.com>
2022-02-07 18:01:56 -0300

    Add ormconfig configuration

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,11 +1,3 @@
 import { createConnection } from 'typeorm'
 
-createConnection({
-  type: 'postgres',
-  url: process.env.DATABASE_URL,
-  entities: [process.env.DATABASE_ENTITIES],
-  migrations: [process.env.DATABASE_MIGRATIONS],
-  cli: {
-    migrationsDir: './src/database/migrations',
-  },
-})
+createConnection()
