5b11f9a84442333110108a1ab29e90a1367213ac
Gabriel Teodoro <oi@gabrielteodoro.com>
2022-02-07 18:18:16 -0300

    Add ssl into ormconfig

--- ormconfig.js
+++ ormconfig.js
@@ -3,8 +3,11 @@ module.exports = {
   url: process.env.DATABASE_URL,
   entities: [process.env.DATABASE_ENTITIES],
   migrations: [process.env.DATABASE_MIGRATIONS],
+  ssl: true,
   extra: {
-    ssl: true,
+    ssl: {
+      rejectUnauthorized: false,
+    },
   },
   cli: {
     migrationsDir: './src/database/migrations',
