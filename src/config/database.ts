82321b206aa14028173cd8ee091ba024e0fdcf1e
Gabriel Teodoro <oi@gabrielteodoro.com>
2022-02-07 17:39:42 -0300

    Add heroku configuration

--- src/config/database.ts
+++
@@ -1,5 +0,0 @@
-export const DATABASE_PORT = Number(process.env.DATABASE_PORT)
-export const DATABASE_NAME = String(process.env.DATABASE_NAME)
-export const DATABASE_HOST = String(process.env.DATABASE_HOST)
-export const DATABASE_USERNAME = String(process.env.DATABASE_USERNAME)
-export const DATABASE_PASSWORD = String(process.env.DATABASE_PASSWORD)
--- src/database/index.ts
+++ src/database/index.ts
@@ -1,22 +1,3 @@
-import {
-  DATABASE_NAME,
-  DATABASE_HOST,
-  DATABASE_PASSWORD,
-  DATABASE_PORT,
-  DATABASE_USERNAME,
-} from '@/config'
 import { createConnection } from 'typeorm'
 
-createConnection({
-  type: 'postgres',
-  port: DATABASE_PORT,
-  host: DATABASE_HOST,
-  database: DATABASE_NAME,
-  password: DATABASE_PASSWORD,
-  username: DATABASE_USERNAME,
-  entities: ['./src/app/core/**/infra/entities/*.ts'],
-  migrations: ['./src/database/migrations/*.ts'],
-  cli: {
-    migrationsDir: './src/database/migrations',
-  },
-})
+createConnection()
